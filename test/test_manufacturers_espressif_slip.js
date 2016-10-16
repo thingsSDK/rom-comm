c0adcbf594159de9b060e24d3408c099739b0b1a
craigsdennis <craig@teamtreehouse.com>
2016-10-16 00:20:31 -0700

    Tests escaping

--- test/test_manufacturers_espressif_slip.js
+++ test/test_manufacturers_espressif_slip.js
@@ -2,7 +2,7 @@
 
 const assert = require("chai").assert;
 const Rx = require("rxjs/Rx");
-// TODO: Fragile!
+// TODO: Fragile import!
 const slip = require("../manufacturers/espressif/slip");
 const FE = slip.CODES.frameEnd;
 
@@ -16,7 +16,23 @@ describe("decodeStream", () => {
         slip.decodeStream(source$)
             .subscribe(x => results.push(x));
 
+        assert.equal(results.length, 2);
         assert.deepEqual(results[0], [1, 2, 3]);
         assert.deepEqual(results[1], [4, 5, 6]);
+    });
+
+    it("unescapes frame markers", () => {
+        const source$ = Rx.Observable.from([FE, 1, 2,
+                                            slip.CODES.frameEscape, slip.CODES.transposedFrameEnd,
+                                            3, 4,
+                                            slip.CODES.frameEscape, slip.CODES.transposedFrameEscape,
+                                            FE]);
+
+        slip.decodeStream(source$)
+            .subscribe(x => assert.deepEqual(
+                x,
+                [1, 2, FE, 3, 4, slip.CODES.frameEscape]
+            )
+        );
     });
 });
